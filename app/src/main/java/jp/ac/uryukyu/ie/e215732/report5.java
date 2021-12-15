77771cca84c222472eba9420524e4ad1c231ec36
Genki Sakumoto <e215732@ie.u-ryukyu.ac.jp>
2021-12-15 21:17:33 +0900

    Lesson1

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e215732/report5.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e215732;
+
+public class report5 {
+    public static void main(String[] args) {
+
+        String str = null;
+        
+        System.out.println(str.length()); 
+    
+    }
+}
