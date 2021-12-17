b3678767ad8aa768a21ff48bce768342461c467a
Genki Sakumoto <e215732@ie.u-ryukyu.ac.jp>
2021-12-17 18:13:28 +0900

    Lesson2

--- app/src/main/java/jp/ac/uryukyu/ie/e215732/report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215732/report5.java
@@ -1,11 +1,20 @@
 package jp.ac.uryukyu.ie.e215732;
 
+
+
 public class report5 {
     public static void main(String[] args) {
 
-        String str = null;
-        
-        System.out.println(str.length()); 
+        try {
+            //処理したいコード
+            String str = null;
+            System.out.println(str.length()); 
+
+        } catch (NullPointerException e) {
+            //NullPointerExceptionが起きたときの処理
+            System.out.println("発動しました");
+             System.out.println(e.getMessage());
+        }
     
     }
 }
